50583da6080981455a55a3c041a218e67c87db77
Rm9tDVAM <u1exite.znd@gmailcom>
2020-01-24 14:48:38 +0900

    MethodLesson8更新

--- java/0124/MethodLesson8.java
+++ java/0124/MethodLesson8.java
@@ -4,8 +4,8 @@ public class MethodLesson8{
 	public static void main(String[] args){
 		System.out.print("文字を入れてね>");
 		String str=new Scanner(System.in).nextLine();
-		reverseWord(str);	
-		System.out.println(str);
+		System.out.println("逆さにしたよ!");
+		System.out.println(reverseWord(str));	
 	}
 	public static String reverseWord(String word){
 		char[] str=new char[word.length()];
