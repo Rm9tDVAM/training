bcb3d14b1b49fae251fb10a5d8bb6cb281284e84
Rm9tDVAM <u1exite.znd@gmailcom>
2020-02-04 12:05:18 +0900

    更新

--- java/kita/method/Q6_5.java
+++ java/kita/method/Q6_5.java
@@ -1,10 +1,36 @@
-public class Q6_5{
+/*
+練習問題 6 - 5
+サイズを示す数値と、表示する文字を引数とし、三角形を表示するメソッドを作成しなさい。
+このメソッドを使用して、サイズと文字の異なる３つの三角形を表示しなさい。
+※ 	サイズを示す引数は int 型、表示する文字の引数は char 型にする。
+
+output
+$ java Q6_5
+#
+##
+###
+
+$
+$$
+$$$
+$$$$
+$$$$$
+
+&
+&&
+&&&
+&&&&
+&&&&&
+&&&&&&
+&&&&&&&
+*/
+	public class Q6_5{
 	public static void main(String[] args){
-		printInt(3,'#');
-		printInt(5,'$');
-		printInt(7,'&');
+		printChar(3,'#');
+		printChar(5,'$');
+		printChar(7,'&');
 	}
-	public static void printInt(int num,char printChar){
+	public static void printChar(int num,char printChar){
 		for(int i=0;i<num;i++){
 			for(int j=0;j<i+1;j++){
 				System.out.print(printChar);
