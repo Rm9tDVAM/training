3bc9e25581eb0e5dc8a66ba413b79b74eda9460c
Rm9tDVAM <u1exite.znd@gmailcom>
2020-02-11 09:56:45 +0900

    new

--- java/util/Common.java
+++ java/util/Common.java
@@ -1,6 +1,36 @@
 package util;
 import java.util.*;
 public class Common{
+//int
+//数値を文字列に変換する
+	public static String intToString(int num){
+		return String.valueOf(num);
+	}
+//数値の桁数を取得する
+	public static int intLength(int num){
+		return String.valueOf(num).length();
+	}
+//数値を桁ごとに配列に収める
+//int[] temp=util.Common.intToiArr(8754198);
+//[8, 7, 5, 4, 1, 9, 8]
+	public static int[] intToiArr(int num){
+		int temp=intLength(num);
+		int j=1;
+		int[] arr=new int[temp];
+		for(int i=0;i<arr.length-1;i++){
+			j*=10;
+		}
+		for(int i=0;i<arr.length;i++,j/=10){
+			arr[i]=num/j;
+			num%=j;
+		}
+		return arr;
+	}
+
+//char
+
+//String
+
 	public static void arrReverse(int[] arr){
 		for(int i=0;i<arr.length/2;i++){
 			int temp=arr[i];
