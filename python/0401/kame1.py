177bb4125e218ad5e22f3e0466d913da7b6d2f2c
Rm9tDVAM <u1exite.znd@gmailcom>
2020-04-01 15:04:36 +0900

    new

---
+++ python/0401/kame1.py
@@ -0,0 +1,6 @@
+import turtle
+t=turtle.Turtle()
+t.shape('turtle')
+t.circle(100)
+turtle.mainloop()
+
---
+++ python/0401/kame4.py
@@ -0,0 +1,16 @@
+import turtle
+n=144
+l=300
+t=turtle.Turtle()
+t.shape('turtle')
+t.forward(l)
+t.right(n)
+t.forward(l)
+t.right(n)
+t.forward(l)
+t.right(n)
+t.forward(l)
+t.right(n)
+t.forward(l)
+turtle.mainloop()
+
