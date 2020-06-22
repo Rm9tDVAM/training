dfebc36026508df7f67ef8ea8163421bb203ddac
Rm9tDVAM <u1exite.znd@gmail.com>
2020-06-23 08:38:12 +0900

    update

--- paiza/Main.java
+++ paiza/Main.java
@@ -1,10 +1,11 @@
-import java.util.*;
+import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int x=sc.nextInt();
-        int y=sc.nextInt();
-        System.out.printf("%d",x*y);
+        double y=sc.nextInt();
+        double z=sc.nextInt();
+        System.out.print((int)((x*(z/100))-(x*(y/100))));
         sc.close();
     }
 }
---
+++ paiza/memo.md
@@ -0,0 +1,21 @@
+# memo
+## import
+~~~java
+import java.util.*;
+import java.util.Scanner;
+~~~
+## Class Main
+~~~java
+public class Main{
+}
+~~~
+## main
+~~~java
+public static void main(String[] args){
+}
+~~~
+## Scanner
+~~~java
+Scanner sc=new Scanner(System.in);
+sc.close();
+~~~
