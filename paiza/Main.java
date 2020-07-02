f98d2d42bf4dcd67636248c843ee0e89a3cf00de
Rm9tDVAM <u1exite.znd@gmail.com>
2020-07-02 10:00:22 +0900

    update

--- paiza/Main.java
+++ paiza/Main.java
@@ -3,16 +3,14 @@ import java.util.stream.*;
 public class Main{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int ans=0;
         int n=sc.nextInt();sc.nextLine();
         String str=sc.nextLine();
         List<Integer> intList=Arrays.stream(str.split(" "))
             .map(Integer::parseInt)
             .collect(Collectors.toList());
+        int ans=n*intList.get(2);;
         if(n>=intList.get(0)){
             ans=n*intList.get(1);
-        }else{
-            ans=n*intList.get(2);
         }
         System.out.print(ans);
         sc.close();
