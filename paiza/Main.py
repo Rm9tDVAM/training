e2b421ede368016731abce272c487ebcf0985308
Rm9tDVAM <u1exite.znd@gmail.com>
2020-07-12 13:14:59 +0900

    update

--- paiza/Main.py
+++ paiza/Main.py
@@ -1,5 +1,9 @@
 # Comment
+''' 0712
+list=[len(input()) for i in range(3)]
+print(max(list))
 '''
+''' 0711
 val=input()
 print("*"*int(val))
 '''
--- paiza/Main/Main/Program.cs
+++ paiza/Main/Main/Program.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Main{
     class Program{
         static void Main(string[] args){
-            Console.WriteLine("Hello World!");
+            var list = new List<int>();
+            for(int i = 0; i < 3; i++)
+            {
+                list.Add(System.Console.ReadLine().Length);
+            }
+
+            Console.WriteLine(list.Max());
         }
     }
 }
