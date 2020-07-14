9d5678155c85494252a35e6cbc55bc20b4baa3e0
nojima <u1exite.znd@gmail.com>
2020-07-15 00:57:36 +0900

    update

--- paiza/Main.java
+++ paiza/Main.java
@@ -1,29 +1,31 @@
 import java.util.*;
+import java.util.stream.*;
 public class Main{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        // 1<=N<=1000, 各レシートの数
         int N=sc.nextInt();sc.nextLine();
-        String[] sArr=new String[N];
-        for(int i=0;i<sArr.length;i++){
-            sArr[i]=sc.nextLine();
+        String str=sc.nextLine();
+        List<String> sList=Arrays.stream(str.split(" "))
+            .collect(Collectors.toList());
+        // 点数 10 倍カードがある場合、総合点は上記の合計点の 10 倍とする。
+        int mag=1;
+        if(sList.contains("x10")){
+            sList.remove(sList.indexOf("x10"));
+            mag=10;
         }
-        // 1<=d_i<=31, 日付
-        // 1<=p_i<=10000, 購入金額
-        int point=0;
-        for(int i=0;i<sArr.length;i++){
-            String[] tArr=sArr[i].split(" ");
-            String d_i=tArr[0];
-            int p_i=Integer.parseInt(tArr[1]);
-            if(d_i.contains("5")){
-                point+=(int)Math.floor(p_i*0.05);
-            }else if(d_i.contains("3")){
-                point+=(int)Math.floor(p_i*0.03);
-            }else{
-                point+=(int)Math.floor(p_i*0.01);
-            }
+        List<Integer> iList = sList.stream()
+            .map(Integer::parseInt)
+            .collect(Collectors.toList());
+        // 点数 0 カードが ある場合、点数カードの中で最大の点を持つカードを 0 点にする。
+        if(sList.contains("0")){
+            iList.remove(iList.indexOf(Collections.max(iList)));
         }
-        System.out.print(point);
+        // 点数カードの合計点を計算する。
+        int sum=0;
+        for(int i:iList){
+            sum+=i;
+        }
+        System.out.print(sum*mag);
         sc.close();
     }
 }
