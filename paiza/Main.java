ad2d4d3dfab28bd80644f6aa70dc6016abad6e94
nojima <u1exite.znd@gmail.com>
2020-07-17 00:16:56 +0900

    update

--- paiza/Main.java
+++ paiza/Main.java
@@ -2,34 +2,38 @@ import java.util.*;
 public class Main{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        // 0<=X,Y,Z<=3,000
-        int X=sc.nextInt(); // 9-17
-        var Y=sc.nextInt(); // 17-22
-        int Z=sc.nextInt(); // 22-09
-        // 1<=N<=100
-        int N=sc.nextInt();
-        // 0<=S<T<23
-        int[] iArr=new int[24];
-        for(int i=0;i<iArr.length;i++){
-            if(i<=8){
-                iArr[i]=Z;
-            }else if(i<=16){
-                iArr[i]=X;
-            }else if(i<=21){
-                iArr[i]=Y;
-            }else{
-                iArr[i]=Z;
-            }
-        }
-        int sum=0;
+        int N=sc.nextInt();sc.nextLine();
+        Map<String,Integer> map=new HashMap<>();
         for(int i=0;i<N;i++){
-            int S=sc.nextInt();
-            int T=sc.nextInt();
-            for(int j=S;j<T;j++){
-                sum+=iArr[j];
+            String str=sc.nextLine();
+            if(map.containsKey(str)){
+                map.put(str,map.get(str)+1);
+            }else{
+                map.put(str,1);
             }
         }
-        System.out.print(sum);
+        int count=0;
+        for(String key:map.keySet()){
+            String str;
+            String t_i=key.substring(0,1);
+            if(t_i.equals("L")){
+                str=key.substring(2)+" R";
+            }else{
+                str=key.substring(2)+" L";
+            }
+            System.out.println(str);
+            if(map.containsKey(str)){
+                int temp=1;
+                while(true){
+                    if(map.get(key)/temp==map.get(str)/temp){
+                        temp++;
+                    }else{
+                        count+=temp-1;
+                    }
+                }
+            }
+        }
+        System.out.print(count);
         sc.close();
     }
 }
