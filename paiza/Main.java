80ee907824cb8c53ad5209cbc4deef1ca96cdc93
nojima <u1exite.znd@gmail.com>
2020-07-19 17:00:45 +0900

    update

--- paiza/Main.java
+++ paiza/Main.java
@@ -2,26 +2,26 @@ import java.util.*;
 public class Main{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        List<String> besList=new ArrayList<String>(Arrays.asList("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"));
-        List<String> afsList1=new ArrayList<String>(Arrays.asList("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"));
-        List<String> afsList2=new ArrayList<String>(Arrays.asList("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"));
+        int ATK=sc.nextInt();
+        int DEF=sc.nextInt();
+        int AGI=sc.nextInt();
         int N=sc.nextInt();sc.nextLine();
-        for(int i=0;i<N;i++){
-            afsList1.add(0,afsList1.get(afsList1.size()-1));
-            afsList1.remove(afsList1.size()-1);
-        }
-        for(int i=0;i<N;i++){
-            afsList2.add(afsList2.size(),afsList2.get(0));
-            afsList2.remove(0);
-        }
-        String S=sc.nextLine();
-        String[] sArr=S.split("");
-        for(int i=0,n=0;i<sArr.length;i++){
-            n=besList.indexOf(sArr[i]);
-            if(i%2==0){
-                System.out.print(afsList1.get(n));
-            }else{
-                System.out.print(afsList2.get(n));
+        for(int i=0,count=0;i<N;i++){
+            String[] evoArr=sc.nextLine().split(" ");
+            int MINATK=Integer.parseInt(evoArr[1]);
+            int MAXATK=Integer.parseInt(evoArr[2]);
+            int MINDEF=Integer.parseInt(evoArr[3]);
+            int MAXDEF=Integer.parseInt(evoArr[4]);
+            int MINAGI=Integer.parseInt(evoArr[5]);
+            int MAXAGI=Integer.parseInt(evoArr[6]);
+            if((MINATK<=ATK&&ATK<=MAXATK)&&
+               (MINDEF<=DEF&&DEF<=MAXDEF)&&
+               (MINAGI<=AGI&&AGI<=MAXAGI)){
+                   System.out.println(evoArr[0]);
+                   count++;
+            }
+            if(i+1==N&&count==0){
+                System.out.println("no evolution");
             }
         }
         sc.close();
