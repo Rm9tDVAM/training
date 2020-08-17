ef4d0367d3aeaf7d9eeb8a04262b76bbafbee61d
nojima <u1exite.znd@gmail.com>
2020-08-17 13:58:45 +0900

    update

--- paiza/Main.java
+++ paiza/Main.java
@@ -4,12 +4,14 @@ class c{
         //System.out.println(obj.getClass().getName());
         if(obj.getClass().isArray()){
             if(obj instanceof int[]){
+                System.out.println("c.print int[]");
                 for(int i:((int[])obj)){
                     System.out.print(i+" ");
                 }
             }
             if(obj instanceof int[][]){
                 int[][] intArray=((int[][])obj);
+                System.out.println("c.print int[][]");
                 for(int i=0;i<intArray.length;i++){
                     for(int j=0;j<intArray[i].length;j++){
                         System.out.printf("%3d",intArray[i][j]);
@@ -18,11 +20,13 @@ class c{
                 }
             }
         }else if(obj instanceof List){
+            System.out.println("c.print List");
             List<?> list=(List<?>)obj;
             for(int i=0;i<list.size();i++){
                 System.out.println(list.get(i));
             }
         }else{
+            System.out.println("c.print");
             System.out.println(obj);
         }
     }
