6c733f6a3ed1411e51af406006a3d90d7d55ef89
nojima <u1exite.znd@gmail.com>
2020-08-20 11:25:30 +0900

    update

--- paiza/Main.java
+++ paiza/Main.java
@@ -2,59 +2,84 @@ import java.util.*;
 public class Main{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int machineNumber=sc.nextInt();
-        int sweetTreatNumber=sc.nextInt();
-        int[] containerNumber=new int[machineNumber];
-        for(int i=0;i<containerNumber.length;i++){
-            containerNumber[i]=sc.nextInt();
+        int instructionNumber=sc.nextInt();sc.nextLine();
+        String[] instructionNumberArray=new String[instructionNumber];
+        for(int i=0;i<instructionNumber;i++){
+            instructionNumberArray[i]=sc.nextLine();
         }
         sc.close();
-        int[] contentArray=new int[containerNumber.length];
-        int[] remainderArray=new int[containerNumber.length];
-        boolean[] remainderBoolArray=new boolean[containerNumber.length];
-        for(int i=0;i<containerNumber.length;i++){
-            contentArray[i]=sweetTreatNumber/containerNumber[i];
-            remainderArray[i]=sweetTreatNumber%containerNumber[i];
-            remainderBoolArray[i]=false;
-        }
-        int remainderMin=1000;
-        for(int i=0;i<remainderArray.length;i++){
-            if(remainderArray[i]<remainderMin){
-                remainderMin=remainderArray[i];
+        Computer computer=new Computer();
+        for(int i=0;i<instructionNumber;i++){
+            String[] temporaryArray=instructionNumberArray[i].split(" ");
+            switch(temporaryArray[0]){
+                case "ADD":
+                    computer.add(Integer.parseInt(temporaryArray[1]));
+                    break;
+                case "SET":
+                    if(Integer.parseInt(temporaryArray[1])==1){
+                        computer.setVariableOne(Integer.parseInt(temporaryArray[2]));
+                    }else{
+                        computer.setVariableTwo(Integer.parseInt(temporaryArray[2]));
+                    }
+                    break;
+                case "SUB":
+                    computer.sub(Integer.parseInt(temporaryArray[1]));
+                    break;
             }
         }
-        for(int i=0;i<remainderArray.length;i++){
-            if(remainderArray[i]==remainderMin){
-                remainderBoolArray[i]=true;
-            }
-        }
-        int machineMax=0;
-        int answerIndex=0;
-        for(int i=0;i<remainderBoolArray.length;i++){
-            if(remainderBoolArray[i]==true){
-                if(machineMax<containerNumber[i]){
-                    machineMax=containerNumber[i];
-                    answerIndex=i+1;
-                }
-            }
-        }
-        System.out.print(answerIndex);
+        computer.showStatus();
     }
 }
-class c{
+class Computer{
+    private int variableOne;
+    private int variableTwo;
+    public Computer(){
+        this.variableOne=0;
+        this.variableTwo=0;
+    }
+    public int getVariableOne(){
+        return this.variableOne;
+    }
+    public int getVariableTwo(){
+        return this.variableTwo;
+    }
+    public void setVariableOne(int variableOne){
+        this.variableOne=variableOne;
+    }
+    public void setVariableTwo(int variableTwo){
+        this.variableTwo=variableTwo;
+    }
+    public void add(int num){
+        this.variableTwo=variableOne+num;
+    }
+    public void sub(int num){
+        this.variableTwo=variableOne-num;
+    }
+    public void showStatus(){
+        System.out.printf("%d %d%n",variableOne,variableTwo);
+    }
+}
+class C{
     static void print(Object obj){
         //System.out.println(obj.getClass());
         //System.out.println(obj.getClass().getName());
         if(obj.getClass().isArray()){
+            //System.out.println("isArray");
             if(obj instanceof int[]){
-                System.out.println("c.print int[]");
+                System.out.println("C.print int[]");
                 for(int i:((int[])obj)){
                     System.out.print(i+" ");
                 }
             }
+            if(obj instanceof String[]){
+                System.out.println("C.print String[]");
+                for(String s:((String[])obj)){
+                    System.out.println(s+" ");
+                }
+            }
             if(obj instanceof int[][]){
                 int[][] intArray=((int[][])obj);
-                System.out.println("c.print int[][]");
+                System.out.println("C.print int[][]");
                 for(int i=0;i<intArray.length;i++){
                     for(int j=0;j<intArray[i].length;j++){
                         System.out.printf("%3d",intArray[i][j]);
@@ -63,13 +88,13 @@ class c{
                 }
             }
         }else if(obj instanceof List){
-            System.out.println("c.print List");
+            System.out.println("C.print List");
             List<?> list=(List<?>)obj;
             for(int i=0;i<list.size();i++){
                 System.out.println(list.get(i));
             }
         }else{
-            System.out.println("c.print");
+            System.out.println("C.print");
             System.out.println(obj);
         }
     }
