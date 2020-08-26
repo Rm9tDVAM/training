741d9dbb5a662c084c2db4bc58cc28aaffafaf91
nojima <u1exite.znd@gmail.com>
2020-08-26 11:30:05 +0900

    update

--- paiza/Main.java
+++ paiza/Main.java
@@ -2,38 +2,64 @@ import java.util.*;
 public class Main{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        Parity parity=new Parity(sc.nextInt());sc.nextLine();
-        for(int i=0;i<parity.getINPUTLINE();i++){
-            String[] temporaryArray=sc.nextLine().split("");
-            for(int j=0;j<4;j++){
-                parity.setBinaryArray(j,Integer.parseInt(temporaryArray[j]));
-            }
-        }
+        GameScore gameScore=new GameScore(sc.nextLine());
+        gameScore.inputFactor(sc.nextLine());
+        gameScore.inputScore(sc);
         sc.close();
-        for(int i=0;i<4;i++){
-            if(parity.getBinaryArray(i)%2==0){
-                System.out.print(0);
-            }else{
-                System.out.print(1);
-            }
-        }
+        gameScore.showScore();
     }
 }
-class Parity{
-    private final int INPUTLINE;
-    private int[] binaryArray;
-    Parity(int INPUTLINE){
-        this.INPUTLINE=INPUTLINE;
-        this.binaryArray=new int[4];
+class GameScore{
+    private final int PARAMETER;
+    private final int USERS;
+    private final int TOP;
+    private Double[] factor;
+    private int[] score;
+    GameScore(String nextLine){
+        String[] temporaryArray=nextLine.split(" ");
+        this.PARAMETER=Integer.parseInt(temporaryArray[0]);
+        this.USERS=Integer.parseInt(temporaryArray[1]);
+        this.TOP=Integer.parseInt(temporaryArray[2]);
+        this.factor=new Double[PARAMETER];
+        this.score=new int[USERS];
     }
-    public int getINPUTLINE(){
-        return this.INPUTLINE;
+    public void inputFactor(String nextLine){
+        String[] temporaryArray=nextLine.split(" ");
+        for(int i=0;i<temporaryArray.length;i++){
+            this.factor[i]=Double.parseDouble(temporaryArray[i]);
+        }
     }
-    public void setBinaryArray(int index,int value){
-        this.binaryArray[index]+=value;
+    public void inputScore(Scanner sc){
+        double temporaryDouble=0;
+        for(int i=0;i<USERS;i++){
+            String[] temporaryArray=sc.nextLine().split(" ");
+            for(int j=0;j<this.PARAMETER;j++){
+                temporaryDouble+=Double.parseDouble(temporaryArray[j])*this.factor[j];
+            }
+            score[i]=(int)Math.round(temporaryDouble);
+            temporaryDouble=0;
+        }
     }
-    public int getBinaryArray(int index){
-        return this.binaryArray[index];
+    public void showScore(){
+        for(int i=0;i<this.score.length-1;i++){
+            for(int j=i+1;j<this.score.length;j++){
+                if(this.score[i]<this.score[j]){
+                    int temporaryInt=this.score[i];
+                    this.score[i]=this.score[j];
+                    this.score[j]=temporaryInt;
+                }
+            }
+        }
+        for(int i=0;i<this.TOP;i++){
+            System.out.println(this.score[i]);
+        }
+    }
+    public void showStatus(){
+        System.out.println(this.PARAMETER);
+        System.out.println(this.USERS);
+        System.out.println(this.TOP);
+        System.out.println(Arrays.toString(factor));
+        System.out.println(Arrays.toString(score));
     }
 }
 class C{
